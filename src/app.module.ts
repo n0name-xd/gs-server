6e5910dddac9df41cf66f8f8277b1b7bd3de87ee
Evgeny_Grinchuk <n0name-xd@inbox.ru>
2023-11-11 18:55:01 +0300

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { User } from './users/models/user.model';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { User } from './users/models/user.model';
       autoLoadModels: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
